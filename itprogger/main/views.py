a38e2befeeb7cd902000984613545a4b922773c4
SergeyZatikian <3605066@gmail.com>
2023-07-05 18:25:58 +0300

    clean

--- itprogger/main/views.py
+++ itprogger/main/views.py
@@ -13,13 +13,5 @@ def about(request):
     return(render(request, 'main/about.html'))
 def joke(request):
     return(render(request, 'main/joke.html'))
-def yes_answer(request):
-    return(render(request, 'main/yes_answer.html'))
-def no_answer(request):
-    return(render(request, 'main/no_answer.html'))
-def kostya(request):
-    return(render(request, 'main/kostya.html'))
-def my_itmo(request):
-    return(render(request, 'main/my_itmo.html'))
 def info_home(request):
     return(render(request, 'main/info_home.html'))
